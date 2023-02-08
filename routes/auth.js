bf045a4ee0529f06714353636a18dd67c1705bb7
Oscar Garcia Ponce <garciaponceoscarabraham1@gmail.com>
2023-02-08 01:04:03 -0600

    Avance 2

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,16 @@ const router = require('express').Router()
 const User = require('../models/User')
 const Joi = require('@hapi/joi')
 const bcrypt = require('bcrypt')
+const jWt = require('jsonwebtoken')
 
 const schemaRegister = Joi.object({
     name: Joi.string().min(6).max(255).require(),
     lastname: Joi.string().max(255).require(),
+    email: Joi.string().max(1024).require(),
+    password: Joi.string().min(6).require()
+})
+
+const schemalogin = Joi.object({
     email: Joi.string().max(1024).require(),
     password: Joi.string().min(6).require()
 })
@@ -46,6 +52,73 @@ router.post('/register', async(req, res) => {
         res.status(400).json({
             message: 'Error al Guardar',
             error
+        })
+    }
+})
+
+router.post('/register', async(req, res) =>  {
+ // Login de ususario
+ const { error } = schemaLogin.validate(req.body)
+ if (error) {
+     return res.status(400).json({
+         error: error.details[0].message
+     })
+ }
+    const isEmailUnique = await User.findOne({ emaiil: req.body.email})
+    if (isEmailUnique) {
+         return res.status(400).json({
+             error: "El correo no existe"
+        })
+    }
+
+    const validPassword = await bcrypt.compare(rqe.body.password, isEmailUnique.password)
+
+    if (validPassword) {
+        return res.status(400).json({
+            error: "Password Incorrecto"
+       })
+    }
+
+
+    const token = jWt.sign({
+        name: isEmailUnique.name,
+        id: isEmailUnique.id
+    }, process.env.TOKEN_SECRET)
+
+    res.header('auth-token', token).json({
+        error: null,
+        data: { token }
+    })
+})
+
+router.get('/getallusers', async (req, res) => {
+    const users = await User.find()
+
+    if(users) {
+        res.json({
+            error: null,
+            data: users
+        })
+    }else{
+        return res.status(400).json({
+            error: "No hay usuarios"
+        })
+    }
+})
+
+router.post('/eraseuser', async (req, res) => {
+    const id = req.body.id
+
+    const erased = await User.findByIdAndDelete(id)
+
+    if(erased) {
+        res.json({
+            error: null,
+            message: "Borrado Satisfactoriamente"
+        })
+    }else{
+        return res.status(400).json({
+            error: "No se pudo borrar el usuario"
         })
     }
 })
